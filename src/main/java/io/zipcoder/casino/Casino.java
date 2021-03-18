f9af1266a46b73b9987d276b4e36d7d29739b017
MNinh <msninh2@gmail.com>
2021-03-18 16:38:53 -0400

    working on menus and connecting everything

--- src/main/java/io/zipcoder/casino/Casino.java
+++ src/main/java/io/zipcoder/casino/Casino.java
@@ -1,28 +1,37 @@
 package io.zipcoder.casino;
 import io.zipcoder.casino.CardGames.Deck;
 import io.zipcoder.casino.DiceGames.Craps.Craps;
+import io.zipcoder.casino.Menus.WelcomeMenu;
 import io.zipcoder.casino.Player;
 
 
 import io.zipcoder.casino.CardGames.Blackjack;
 
 import io.zipcoder.casino.utilities.Console;
+import io.zipcoder.casino.utilities.Membership;
 
 
 public class Casino {
 
     private static Console console = new Console(System.in, System.out);
+    private static String firstName;
+    private static String lastName;
+    private static Integer age;
+    private static Double balance;
 
     public static void main(String[] args) {
+        WelcomeMenu welcome = new WelcomeMenu(console);
+        Membership membership = new Membership();
+//        membership.membersList(firstName, lastName, age, balance);
+        boolean enterCasino = true;
+
+        while(enterCasino){
+            welcome.menuRun();
+        }
 
     }
 
-    public static Player playerInfo() {
-        String firstName = console.getInput("Teller: What's your first name? ");
-        String lastName = console.getInput("Teller: What's your last name? ");
-        int age = console.getInteger("Teller: How old are you? ");
-        return new Player(firstName, lastName, age, 0.0);
-    }
+
 
 
 
