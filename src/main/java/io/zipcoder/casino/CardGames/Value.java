acd7fc1d02b268589aaea761317d0311c3df516b
Jbanksalpha <78812718+Jbanksalpha@users.noreply.github.com>
2021-03-17 23:25:04 -0400

    made changes and updates to blackjack

--- src/main/java/io/zipcoder/casino/CardGames/Value.java
+++ src/main/java/io/zipcoder/casino/CardGames/Value.java
@@ -1,5 +1,5 @@
 package io.zipcoder.casino.CardGames;
 
 public enum Value {
-    TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE;
+    TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, J, Q, K, A;
 }
